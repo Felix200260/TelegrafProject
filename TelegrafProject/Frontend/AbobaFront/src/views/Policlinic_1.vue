ee4ef5e891703cc5f62d002bff3abcdef06c8190
Felix <korchevskiy_fv@medinfocenter.ru>
2025-07-04 17:14:50 +0500

    -

---
+++ TelegrafProject/Frontend/AbobaFront/src/views/Policlinic_1.vue
@@ -0,0 +1,21 @@
+﻿<template>
+  <h1>snjbkansbkm</h1>
+</template>
+
+<script setup lang="ts">
+
+const emit = defineEmits(['update:isCLickPoliclinic_1'])
+
+const props = defineProps({
+  isCLickPoliclinic_1: Boolean,
+})
+
+const closeModal = () => {
+  emit('update:isCLickPoliclinic_1', false)
+}
+
+</script>
+
+<style scoped>
+
+</style>
